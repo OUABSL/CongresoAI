34152b7ee3956fa9f0969fe38b0d64ca7c30b353
OUABSL <ouael999@gmail.con>
2024-02-27 00:37:41 +0100

    minima mejora de frontend

--- backend/src/routes/users.py
+++ backend/src/routes/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Blueprint
+from flask import Flask, request, Blueprint, jsonify, make_response
 from flask_login import UserMixin, LoginManager, login_user
 from werkzeug.security import generate_password_hash
 import sys,os
@@ -9,15 +9,12 @@ from werkzeug.security import check_password_hash
 from flask_login import logout_user
 
 
-
 users_bp = Blueprint('users', __name__)
 
 mycol = mongo.db.users
 
-
 login_manager = LoginManager()
 login_manager.init_app(users_bp)
-
 
 @login_manager.user_loader
 def load_user(username):
@@ -25,7 +22,6 @@ def load_user(username):
     if not u:
         return None
     return User(u['email'],u['username'],u['password'],u['fullname'],u['birthdate'],u['phonenumber'],u['interestarea'])
-
 
 @users_bp.route('/login', methods=['POST'])
 def login():
@@ -37,10 +33,9 @@ def login():
     user = mycol.find_one({'username': username})
 
     if user and check_password_hash(user['password'], password):
-        return 'Login successful!'
+        return make_response(jsonify({'message': 'Login successful!'}), 200)
     else:
-        return 'Wrong username or password!'
-    
+        return make_response(jsonify({'message': 'Wrong username or password!'}), 401)
 
 @users_bp.route('/signup', methods=['POST'])
 def SignUp():
@@ -55,17 +50,17 @@ def SignUp():
     interestarea = data.get('interestarea')
 
     user = mycol.find_one({'username': username})
+
     if user:
-        return 'Username already exists!'
+        return make_response(jsonify({'message':'Username already exists!'}), 400)
     else:
         new_user = User(email, username, password, fullname, birthdate, phonenumber, interestarea)
         mycol.insert_one({'email': new_user.email, 'username': new_user.username, 'password': new_user.password,
                           'fullname': new_user.fullname, 'birthdate': new_user.birthdate, 'phonenumber': new_user.phonenumber,
                           'interestarea': new_user.interestarea})
-        return 'Registration successful!'
-    
+        return make_response(jsonify({'message':'Registration successful!'}), 201)
 
 @users_bp.route('/logout')
 def logout():
     logout_user()
-    return 'Logged out!'
+    return jsonify({'message':'Logged out!'})
