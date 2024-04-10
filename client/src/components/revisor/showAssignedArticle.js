ec100981b37bcda47cb195ccb5675dc07431d779
OUABSL <ouael999@gmail.con>
2024-04-10 02:57:18 +0200

    Agregar/Actualizar la revision de un articulo

--- client/src/components/revisor/showAssignedArticle.js
+++ client/src/components/revisor/showAssignedArticle.js
@@ -6,9 +6,93 @@ import RegenerationModal from './regeneratePreEvaluation'
 import ReassignateReviewButton from './reAssignateReviewer';
 
 // Constant criteria and scale
-const CRITERIA = ['Motivation:', 'Novelty:', 'Clarity:', 'Grammar and Style:', 'Typos and Errors:'];
+const CRITERIA = ['Motivation', 'Novelty', 'Clarity', 'Grammar and Style', 'Typos and Errors'];
 const SCALE = ['YES', 'Can be improved', 'Must be Improved', 'Not Applicable'];
 const SECTION_ORDER = ["Abstract", "Introduction", "Related Word", "Conclusions and future works"];
+const defaultReviewSection = {
+  'Motivation': '',
+  'Novelty': '', 
+  'Clarity': '',
+  'Grammar and Style': '',
+  'Typos and Errors': '',
+  'comment': ''
+};
+
+
+const handleDownload = async (fileUrl, filename, filetype) => {
+  try {
+      const response = await fetch(`/api/v1${fileUrl}`);
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+      const blob = await response.blob();
+      const fileBlob = new Blob([blob], { type: `application/${filetype}` });
+      const url = window.URL.createObjectURL(fileBlob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `${filename}.${filetype}`);
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+  } catch (error) {
+      console.error(`Error fetching ${filetype.toUpperCase()} file:`, error);
+  }
+};
+
+
+const DownloadArticle = ({pdf, zip, title}) =>{
+    
+  const handleDownloadPdf = async (pdfUrl, filename) => {
+      try {
+        const response = await fetch(`/api/v1${pdfUrl}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const blob = await response.blob();
+        const pdfBlob = new Blob([blob], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(pdfBlob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `${filename}.pdf`);
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+      } catch (error) {
+        console.error('Error fetching PDF:', error);
+      }
+    };
+const handleDownloadZip = async (zipUrl, filename) => {
+  try {
+    const response = await fetch(`/api/v1${zipUrl}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${filename}.zip`);
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode.removeChild(link);
+  } catch (error) {
+    console.error('Error fetching Zip:', error);
+  }
+  };
+
+  return (
+
+      <>
+  <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+    <DropdownButton id="dropdown-button" title="Descargar">
+      <Dropdown.Item onClick={() => handleDownloadPdf(pdf, title)}>Descargar PDF</Dropdown.Item>
+      <Dropdown.Item onClick={() => handleDownloadZip(zip, title)}>Descargar ZIP</Dropdown.Item>
+    </DropdownButton>
+  </div>
+    </>
+      )
+};
+
+
 
 const formatPreEvalSection = (preEvalSection) => {
     if(!preEvalSection) return '';
@@ -22,81 +106,78 @@ const formatPreEvalSection = (preEvalSection) => {
     return formattedPreEvalSection;
 };
 
-const handleDownload = async (fileUrl, filename, filetype) => {
-    try {
-        const response = await fetch(`/api/v1${fileUrl}`);
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
-        const blob = await response.blob();
-        const fileBlob = new Blob([blob], { type: `application/${filetype}` });
-        const url = window.URL.createObjectURL(fileBlob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', `${filename}.${filetype}`);
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-    } catch (error) {
-        console.error(`Error fetching ${filetype.toUpperCase()} file:`, error);
-    }
+const Review = ({ reviewData, handleSectionReviewSave }) => {
+  const [review, setReview] = React.useState(reviewData);
+
+  const handleInputChange = (e, criterion) => {
+      const updatedReview = { ...review, [criterion]: e.target.value };
+      setReview(updatedReview);
+      console.log(updatedReview); 
+  };
+
+  const handleClickSaveReview = () => {
+    handleSectionReviewSave(review);
+  };
+
+  return (
+      <div>
+          {CRITERIA.map(criterion => (
+              <Row key={criterion} className="align-items-center my-2">
+                  <Col xs={12} md={4}>
+                      <h6>{criterion}</h6>
+                  </Col>
+                  <Col xs={12} md={8}>
+                {SCALE.map((evalScale, index) => (
+                    <Form.Check 
+                        inline 
+                        label={evalScale} 
+                        name={criterion} 
+                        type="radio" 
+                        id={`radio-${criterion}-${evalScale}`}
+                        value={evalScale} 
+                        checked={review[criterion] === evalScale} 
+                        onChange={(e) => handleInputChange(e, criterion)}
+                        key={`${criterion}-${index}`}
+                    />
+                ))}
+            </Col>
+              </Row>
+          ))}
+          <Form.Group controlId="reviewComment" className="mt-3">
+              <Form.Label>Comentario del Revisor</Form.Label>
+              <Form.Control 
+                as="textarea" 
+                rows={3} 
+                value={review.comment || ''} 
+                onChange={(e) => handleInputChange(e, 'comment')}
+              />
+          </Form.Group>
+          <Button 
+            variant="primary" 
+            onClick={handleClickSaveReview} 
+            className="mt-3"
+          >
+            Guardar Revisión de Sección
+          </Button>
+      </div>
+  );
 };
 
-const Review = ({ reviewData, handleReviewChange }) => {
-    const [review, setReview] = useState(reviewData || {});
-  
-    const handleInputChange = (e, criterion) => {
-        const updatedReview = { ...review, [criterion]: e.target.value };
-        setReview(updatedReview);
-        handleReviewChange(updatedReview);
-    };
-  
-    return (
-      <div>
-        {CRITERIA.map(criterion => (
-          <Row key={criterion} className="align-items-center my-2">
-            <Col xs={12} md={4}>
-              <h6>{criterion}</h6>
-            </Col>
-            <Col xs={12} md={8}>
-              {SCALE.map((evalScale) => (
-                <Form.Check 
-                  inline 
-                  label={evalScale} 
-                  name={criterion} 
-                  type="radio" 
-                  id={`radio-${criterion}-${evalScale}`}
-                  value={evalScale} 
-                  checked={review[criterion] === evalScale} 
-                  onChange={(e) => handleInputChange(e, criterion)} 
-                />
-              ))}
-            </Col>
-          </Row>
-        ))}
-        <Form.Group controlId="reviewComment" className="mt-3">
-          <Form.Label>Comentario del Revisor</Form.Label>
-          <Form.Control 
-            as="textarea" 
-            rows={3} 
-            value={review.comment || ''} 
-            onChange={(e) => handleInputChange(e, 'comment')}
-          />
-        </Form.Group>
-        <Button variant="primary" onClick={() => handleReviewChange(review)} className="mt-3">
-          Guardar Revisión de Sección
-        </Button>
-      </div>
-    );
-  };
-
-const DisplaySection = ({ section, summarySection, preEvalSection, updateReview }) => {
-  const [reviewSection, setReviewSection] = useState("");
+const DisplaySection = ({ section, summarySection, preEvalSection, actualReviewSection }) => {
+  const [reviewSection, setReviewSection] = useState(defaultReviewSection);
   const [editing, setEditing] = useState(true);
   
-  const handleReviewChange = () => {
-      updateReview(section, reviewSection);
-      setEditing(!editing);
+  if(actualReviewSection!=null){
+    setReviewSection(actualReviewSection);
+  }
+
+
+  const handleSectionReviewSave = (updatedReview) => {
+    setReviewSection(updatedReview);
+    setEditing(!editing);
   };
+
   
   const handleEdit = () => {
       setEditing(true);
@@ -114,78 +195,20 @@ const DisplaySection = ({ section, summarySection, preEvalSection, updateReview 
             <h5>PreEvaluación</h5>
             <p dangerouslySetInnerHTML={{ __html: formattedPreEvalSection }} />
             <h5>Revisión</h5>
-            <Review reviewData={reviewSection} handleReviewChange={handleReviewChange}/>
+            <Review reviewData={reviewSection} handleSectionReviewSave={handleSectionReviewSave}/>
         </Accordion.Body>
     </Accordion.Item>
 );
 }
 
 
-const DownloadArticle = ({pdf, zip, title}) =>{
-    
-    const handleDownloadPdf = async (pdfUrl, filename) => {
-        try {
-          const response = await fetch(`/api/v1${pdfUrl}`);
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          const blob = await response.blob();
-          const pdfBlob = new Blob([blob], { type: 'application/pdf' });
-          const url = window.URL.createObjectURL(pdfBlob);
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', `${filename}.pdf`);
-          document.body.appendChild(link);
-          link.click();
-          link.parentNode.removeChild(link);
-        } catch (error) {
-          console.error('Error fetching PDF:', error);
-        }
-      };
-  const handleDownloadZip = async (zipUrl, filename) => {
-    try {
-      const response = await fetch(`/api/v1${zipUrl}`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `${filename}.zip`);
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
-    } catch (error) {
-      console.error('Error fetching Zip:', error);
-    }
-    };
-
-    return (
-
-        <>
-    <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <DropdownButton id="dropdown-button" title="Descargar">
-        <Dropdown.Item onClick={() => handleDownloadPdf(pdf, title)}>Descargar PDF</Dropdown.Item>
-        <Dropdown.Item onClick={() => handleDownloadZip(zip, title)}>Descargar ZIP</Dropdown.Item>
-      </DropdownButton>
-    </div>
-      </>
-        )
-  };
-
-
-  
 function ShowAssignedArticle() {
     const {sessionToken, logout} = useContext(AuthContext); // Accede a username y sessionToken desde el contexto
     const { username, article_title } = useParams();
     const [article, setArticle] = useState({});
     const [review, setReview] = useState({});
-    const [selectedPdf, setSelectedPdf] = useState(null);
     const sectionOrder = ["Abstract", "Introduction", "Related Word", "Conclusions and future works"];
     const [alert, setAlert] = useState({visible: false, variant: '', message: ''});
-    const criteria = ['Motivation:', 'Novelty:', 'Clarity:', 'Grammar and Style:', 'Typos and Errors:'];
-    const scale = ['YES', 'Can be improved', 'Must be Improved', 'Not Applicable'];
     const navigate = useNavigate();
 
     const goBack = () => {
@@ -252,9 +275,6 @@ function ShowAssignedArticle() {
         console.log(data); 
     };
 
-    const updateReview = (section, reviewSection) => {
-        setReview(previousReview => ({ ...previousReview, [section]: reviewSection }));
-    };
 
     return (
         <>
@@ -290,7 +310,7 @@ function ShowAssignedArticle() {
                         section={section}
                         summarySection={summarySection}
                         preEvalSection={article.evaluation[section]}
-                        updateReview={updateReview}
+                        reviewSection={article.review && article.review[section] ? article.review[section] : null}
                       />
                   ))}
                 </Accordion>
